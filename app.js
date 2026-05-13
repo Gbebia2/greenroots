9bd871f081367d813583deda3f63668a73cb70b9
gbebia2 <gbebia2@gmail.com>
2026-05-13 04:50:39 -0400

    latest-changes

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ const path = require("path");
 const session = require("express-session");
 const flash = require("express-flash");
 const productsData = [
-  { id: 1, name: "Herbal Tea", image: "/images/herbal-tea.jpg" },
-  { id: 2, name: "Fertility Tonic", image: "/images/tonic.jpg" },
-  { id: 3, name: "Herbal Capsules", image: "/images/capsules.jpg" }
+  {
+    id: 1,
+    name: "Smoker's Detox",
+    image: "/images/smokers-detox.png",
+    description: "This potent herbal blend is specifically formulated to support respiratory health, helping to clear and soothe the lungs using natural mullein and peppermint."
+  },
+  {
+    id: 2,
+    name: "Blood and Lungs Detox",
+    image: "/images/blood-lungs.png",
+    description: "A dual-action formula designed to purify the bloodstream and reinforce lung function through a selection of deep-cleansing organic herbs."
+  },
+  {
+    id: 3,
+    name: "Male Libido Super Charge",
+    image: "/images/male-libido.jfif",
+    description: "Experience renewed energy and vitality with this powerful stamina-boosting blend, crafted to support natural hormone balance and performance."
+  },
+  {
+    id: 4,
+    name: "Female Fertility Treatment",
+    image: "/images/female-fertility.jfif",
+    description: "A gentle, nurturing herbal treatment designed to support reproductive wellness and hormonal harmony for women."
+  }
 ];
 
 const app = express();
@@ -30,7 +51,7 @@ app.use(flash());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { page: "home" });
+  res.render("index", { page: "home", products: productsData });
 });
 
 app.get("/about", (req, res) => {
